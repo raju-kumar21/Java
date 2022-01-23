4c6ba965b8345e87d864972e1d84344d33f274eb
raju-kumar21 <97028776+raju-kumar21@users.noreply.github.com>
2022-01-23 23:24:45 +0530

    Add files via upload

---
+++ 1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class  {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextint();
+        int b = sc.nextint();
+        int c = sc.nextint();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
